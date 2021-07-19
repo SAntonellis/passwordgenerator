5998c03b740bf10274c5222507a9d72b594467ad
SophiaA <sophia.antonellis@gmail.com>
2021-07-19 01:23:05 -0400

    added const variables for generating random char and function for pw generation on submit

--- script.js
+++ script.js
@@ -6,3 +6,27 @@ const numbersDOM = document.getElementById('numbers');
 const symbolsDOM = document.getElementById('symbols');
 const generatebtn = document.getElementById('generate');
 const form = document.getElementById('passwordGeneratorForm');
+
+const UPPERCASE_CODES = arrayFromLowToHigh(65, 90);
+ const LOWERCASE_CODES = arrayFromLowToHigh(97, 122);
+ const NUMBER_CODES = arrayFromLowToHigh(48, 57);
+ const SYMBOL_CODES = arrayFromLowToHigh(33, 47)
+   .concat(arrayFromLowToHigh(58, 64))
+   .concat(arrayFromLowToHigh(91, 96))
+   .concat(arrayFromLowToHigh(123, 126));
+ 
+
+ form.addEventListener('submit', (e) => {
+   e.preventDefault();
+   const characterAmount = lengthDOM.value;
+   const includeUppercase = uppercaseDOM.checked;
+   const includeNumbers = numbersDOM.checked;
+   const includeSymbols = symbolsDOM.checked;
+   const password = generatePassword(
+     characterAmount,
+     includeUppercase,
+     includeNumbers,
+     includeSymbols
+   );
+   resultDOM.innerText = password;
+ });
